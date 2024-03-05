f9b2f50c57b7ea5af869fd8c1350ac74a8eb4e03
Rohith Jayawardene <gambol99@gmail.com>
2024-03-05 14:02:38 +0000

    fix: adding the changes to use as a module

--- variables.tf
+++ variables.tf
@@ -14,12 +14,6 @@ variable "external_region" {
   default     = "eu-west-2"
 }
 
-variable "aws_accounts" {
-  description = "A map of the name at the account id"
-  type        = map(string)
-  default     = {}
-}
-
 variable "enable_landing_zone_support" {
   description = "Enable the creation of the support role in the customer account"
   type        = bool
@@ -35,40 +29,35 @@ variable "enable_cost_analysis_support" {
 variable "support_role_name" {
   description = "The name of the role created in the customer account"
   type        = string
-  default     = "LZASupportRole"
+  default     = "AppviaSupportRole"
 }
 
 variable "cost_analysis_role_name" {
   description = "The name of the role used to support the cudos dashboards"
   type        = string
-  default     = "CostAnalysisSupportRole"
+  default     = "AppviaCostAnalysisSupportRole"
 }
 
 variable "landing_zone_policy_name" {
   description = "Customer managed support policy for landing zone"
   type        = string
-  default     = "LZASupportPolicy"
+  default     = "AppviaSupportPolicy"
 }
 
 variable "costs_analysis_policy_name" {
   description = "Customer managed support policy for cost analysis"
   type        = string
-  default     = "CostAnalysisSupportPolicy"
+  default     = "AppviaCostAnalysisSupportPolicy"
 }
 
 variable "cudos_policy_name" {
   description = "Customer managed support policy for dashboards"
   type        = string
-  default     = "CudosSupportPolicy"
+  default     = "AppviaCudosSupportPolicy"
 }
 
 variable "tags" {
   description = "A map of tags to add to the resources"
   type        = map(string)
   default     = {}
-}
-
-variable "region" {
-  description = "The AWS region where to deploy the resources"
-  type        = string
 }
